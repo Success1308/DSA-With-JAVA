11ac7d80133d6898565ed3cf4d2bca84283f7185
Success Ambaliya <success.ambaliya13@gmail.com>
2024-05-13 12:30:16 -0400

    Add files via upload

---
+++ VI. function/q.java
@@ -0,0 +1,87 @@
+import java.util.*;
+public class q {
+
+// Q1 
+    // public static int avg (int a , int b, int c) {
+
+    //     int avgt = (a+b+c)/3;
+    //     return avgt;
+    // }
+
+
+//Q2 
+    // public static boolean isEven(int a){
+    //     boolean even = true;
+    //     if (a%2 == 0){
+    //         even = true;
+    //     } else {
+    //         even = false;
+    //     }
+    //     return even;
+
+    // }
+
+
+//q3 
+
+    // public static boolean isPali(int a){
+    //     int palindrome = a;
+    //     int reverse = 0;
+    //     while(palindrome != 0){
+    //         int rem = palindrome % 10;
+    //         reverse = reverse * 10 + rem;
+    //         palindrome = palindrome /10;
+    //     }
+    //     if (a == reverse ){
+    //         return true;
+    //     } return false;
+    // }
+
+
+//q5
+
+    // public static int sum (int a) {
+    //    int newNum = 0;
+    //     while(a>0){
+    //         int b = a%10;
+    //         newNum += b;
+    //         a/=10;
+    //     }
+    //     return newNum;
+    // }
+
+    
+
+    public static void main(String arg[]){
+// Q1 
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("x : " );
+        // int x = sc.nextInt();
+        // System.out.print("y : " );
+        // int y = sc.nextInt();
+        // System.out.print("z : " );
+        // int z = sc.nextInt();
+        // System.out.print("Average of 3 : " + avg(x,y,z) );
+
+//Q2 
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("e : " );
+        // int e = sc.nextInt();
+        // System.out.print("is even: " + isEven(e) );
+
+//Q3
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("f : " );
+        // int f = sc.nextInt();
+        // System.out.print("is palindrome: " + isPali(f) );
+
+
+//q5
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("h : " );
+        // int h = sc.nextInt();
+        // System.out.print("sum of: " + sum(h) );
+
+    }
+}
